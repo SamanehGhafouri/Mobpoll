2d3568c1f15e4cbb8be94c75b68ea300c7448019
SamanehGhafouri <samaneh2010gh@gmail.com>
2020-11-13 21:15:02 -0500

    changed background image

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreatePollComponent } from './create-poll/create-poll.component';
+import { HelpComponent } from './help/help.component';
+import { AboutComponent } from './about/about.component';
+import { MoreComponent } from './more/more.component';
+import { PublicPollsComponent } from './public-polls/public-polls.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreatePollComponent,
+    HelpComponent,
+    AboutComponent,
+    MoreComponent,
+    PublicPollsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/public-polls/public-polls.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-public-polls',
+  templateUrl: './public-polls.component.html',
+  styleUrls: ['./public-polls.component.css']
+})
+export class PublicPollsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
