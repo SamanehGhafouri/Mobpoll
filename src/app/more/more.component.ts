ae970e3b5457046c11d177e9893985419335f85f
SamanehGhafouri <samaneh2010gh@gmail.com>
2020-11-22 22:46:33 -0500

    more page is using container

--- src/app/more/more.component.ts
+++ src/app/more/more.component.ts
@@ -3,10 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-more',
   templateUrl: './more.component.html',
-  styleUrls: ['../form-header/form-header.component.css', './more.component.css']
+  styleUrls: ['./more.component.css']
 })
 export class MoreComponent implements OnInit {
-
+  moreMessage="Learn More";
   constructor() { }
 
   ngOnInit(): void {
