34983d4f40437e69cc7998dcaa035b949a3a87e8
SamanehGhafouri <samaneh2010gh@gmail.com>
2020-12-28 16:08:46 -0500

    cleaning code in app (root) component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,47 +1,15 @@
-import {Component, ViewEncapsulation} from '@angular/core';
-import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
-import {Router} from "@angular/router";
-import {CreatePollHelpComponent} from "./create-poll-help/create-poll-help.component";
-import {AboutHelpComponent} from "./about-help/about-help.component";
-import {LearnMoreHelpComponent} from "./learn-more-help/learn-more-help.component";
-import {PublicPollsHelpComponent} from "./public-polls-help/public-polls-help.component";
-
+import {Component} from '@angular/core';
 
 
 @Component({
   selector: 'app-root',
-  // encapsulation: ViewEncapsulation.Emulated,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  // styles:[`
-  //   .mobpoll-help-modal .modal-content{
-  //     border-color: red;
-  //     border-radius: 20px;
-  //   }
-  //   .mobpoll-helpss-modal .modal-content{
-  //     border-color: #FF9E13;
-  //   }
-  // `]
+
 })
 export class AppComponent {
-  title = 'Mobpoll-in-Angular';
 
+  constructor() {
+  }
 
-  // constructor(private modalService: NgbModal, public router: Router) {
-  // }
-
-  // helpCom() {
-  //   if (this.router.url == '/') {
-  //     const modalRef = this.modalService.open(CreatePollHelpComponent, {scrollable: true, windowClass:'mobpoll-help-modal'});
-  //
-  //   } else if (this.router.url == '/about') {
-  //     const modalRefAbout = this.modalService.open(AboutHelpComponent, {scrollable: true, windowClass:'mobpoll-helpss-modal'});
-  //   }
-  //   else if (this.router.url == '/learn_more') {
-  //     const modalRefLearnMore = this.modalService.open(LearnMoreHelpComponent, {scrollable: true});
-  //   }
-  //   else if (this.router.url == '/public_polls'){
-  //     const modalRefLearnMore = this.modalService.open(PublicPollsHelpComponent, {scrollable: true});
-  //   }
-  // }
 }
