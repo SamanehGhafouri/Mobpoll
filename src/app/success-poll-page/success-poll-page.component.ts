05464ce8e81e2ecab71f5790236e671a17f9b0d6
SamanehGhafouri <samaneh2010gh@gmail.com>
2021-01-11 22:35:59 -0500

    your poll and iframe fixed

--- src/app/success-poll-page/success-poll-page.component.ts
+++ src/app/success-poll-page/success-poll-page.component.ts
@@ -12,11 +12,17 @@ export class SuccessPollPageComponent implements OnInit {
   pollId: string;
   pollQuestion: string;
   successPollPage = 'success!';
+  yourPoll : string;
+  iFrame: string;
+
   constructor(private route: ActivatedRoute, private service:ServiceService) {
 
     // Getting pollId from the URL
     route.params.subscribe(pollId => {
       this.pollId = pollId['pollId'];
+      this.yourPoll = 'https://www.mobpoll.org/poll/' + this.pollId
+      this.iFrame = `<iframe src="https://www.mobpoll.org/embed/poll/` + this.pollId +
+        `" frameborder="0" width="500" height="500"></iframe>`
 
       // Getting poll using Id
       this.service.getPollById(this.pollId).subscribe(document => {
