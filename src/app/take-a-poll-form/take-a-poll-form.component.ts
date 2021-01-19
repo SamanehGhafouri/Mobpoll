06c2af8598b6e4f92d2129870305a5a9a3fe0015
SamanehGhafouri <samaneh2010gh@gmail.com>
2021-01-19 17:15:57 -0500

    got poll question and poll options in console.log

--- src/app/take-a-poll-form/take-a-poll-form.component.ts
+++ src/app/take-a-poll-form/take-a-poll-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {ServiceService} from "../service.service";
 
 @Component({
   selector: 'app-take-a-poll-form',
@@ -6,8 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./take-a-poll-form.component.css']
 })
 export class TakeAPollFormComponent implements OnInit {
+  pollId : string;
+  pollQuestion : string;
+  pollOptions: [];
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private service: ServiceService) {
+    //Get poll Id from url
+    route.params.subscribe(pollId =>{
+      this.pollId = pollId['pollId']
+      console.log("is that getting the poll Id ----> ", this.pollId)
+
+      // using Poll Id from url to get data from database
+      this.service.getPollById(this.pollId).subscribe(document => {
+        const poll = document.data()
+        this.pollQuestion = poll['pollQuestion']
+        this.pollOptions = poll['options']
+        console.log("what is the poll question? --->  ", this.pollQuestion)
+        console.log("what are the options? ----> ", this.pollOptions)
+      })
+
+    })
+  }
 
   ngOnInit(): void {
   }
