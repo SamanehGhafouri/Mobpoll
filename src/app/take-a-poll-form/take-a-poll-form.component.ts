531cd158f2b5b4fffef431646325c833b754373f
SamanehGhafouri <samaneh2010gh@gmail.com>
2021-01-23 15:52:33 -0500

    got options info in seperated arrays

--- src/app/take-a-poll-form/take-a-poll-form.component.ts
+++ src/app/take-a-poll-form/take-a-poll-form.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {ServiceService} from "../service.service";
+import {FormArray, FormControl, FormGroup, Validators} from "@angular/forms";
+import {animate} from "@angular/animations";
 
 @Component({
   selector: 'app-take-a-poll-form',
@@ -11,6 +13,10 @@ export class TakeAPollFormComponent implements OnInit {
   pollId : string;
   pollQuestion : string;
   pollOptions: [];
+  mainForm: FormGroup;
+  optionId: any[]
+  optionName: any[]
+  optionTally: any
 
   constructor(private route: ActivatedRoute, private service: ServiceService) {
     //Get poll Id from url
@@ -24,13 +30,46 @@ export class TakeAPollFormComponent implements OnInit {
         this.pollQuestion = poll['pollQuestion']
         this.pollOptions = poll['options']
         console.log("what is the poll question? --->  ", this.pollQuestion)
-        console.log("what are the options? ----> ", this.pollOptions)
+
+        // This is getting info from options in our database such as optionName, optionId, tally
+        let option_names = []
+        let option_ids = []
+        let option_tallies = []
+
+        for(let option of this.pollOptions){
+          console.log("I wanna see this? " ,option["optionName"])
+          option_names.push(option["optionName"])
+          this.optionName = option_names
+
+          option_ids.push(option["optionId"])
+          this.optionId = option_ids
+
+          option_tallies.push(option["tally"])
+          this.optionTally = option_tallies
+        }
+        console.log("show me the array of option names? ", this.optionId ,this.optionName, this.optionTally)
+
+
+
       })
 
     })
+
   }
 
   ngOnInit(): void {
+
+    // let options = this.pollOptions;
+    // this.mainForm = new FormGroup({
+    //   this.options: new FormArray([
+    //     new FormControl(''),
+    //     new FormControl(''),
+    //     new FormControl(''),
+    //     new FormControl(''),
+    //     new FormControl('')
+    //   ])
+    //
+    // });
   }
 
 }
