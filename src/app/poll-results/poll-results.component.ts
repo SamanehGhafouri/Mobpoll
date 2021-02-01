f06f8fbc8e0d301d997f56e9caf0da5cc4ad4e2c
SamanehGhafouri <samaneh2010gh@gmail.com>
2021-01-31 21:44:22 -0500

    all page results features working

--- src/app/poll-results/poll-results.component.ts
+++ src/app/poll-results/poll-results.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ServiceService} from "../service.service";
 import {ActivatedRoute} from "@angular/router";
 
@@ -14,8 +14,7 @@ export class PollResultsComponent implements OnInit {
   pollOptions: [];
   totalVotes: number;
 
-
-  constructor(private route: ActivatedRoute, private service: ServiceService, ) {
+  constructor(private route: ActivatedRoute, private service: ServiceService,) {
     //Get poll Id from url
     route.params.subscribe(pollId => {
       this.pollId = pollId['pollId']
@@ -31,7 +30,18 @@ export class PollResultsComponent implements OnInit {
     });
   }
 
-  ngOnInit(): void {
+  getCustomColor(optionId) {
+    let colorDict = {
+      'blue': 'rgb(65, 167, 251)',
+      'green': 'rgb(127, 201, 28)',
+      'orange': 'rgb(253, 150, 0)',
+      'red': 'rgb(252, 44, 27)',
+      'gray': 'rgb(190, 204, 212)'
+    };
+    let arr = [colorDict['blue'], colorDict['green'], colorDict['gray'], colorDict['orange'], colorDict['red']];
+    let target = optionId % arr.length;
+    return arr[target];
   }
 
+  ngOnInit(): void {}
 }
