3f9a85714c06b0ec32e489324f6d3b84b6f6bd95
SamanehGhafouri <samaneh2010gh@gmail.com>
2021-02-08 18:17:41 -0500

    buttons on navbar will remain highlighted if it is selected

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+import {Router, RouterEvent} from "@angular/router";
+import {filter} from "rxjs/operators";
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,6 +9,23 @@ import {Component} from '@angular/core';
 })
 export class NavBarComponent {
 
-  constructor() {
+  current_url_path: string;
+
+  constructor(private router: Router) {
+
+    let route_event_filter = filter((e): e is RouterEvent => e instanceof RouterEvent)
+    router.events.subscribe((val) => {
+      router.events.pipe(route_event_filter).subscribe((e: RouterEvent) => {
+        this.current_url_path = e.url;
+      });
+    })
+  }
+
+  onClick(url_path: string) {
+    if (this.current_url_path == url_path) {
+      return 'currently-selected';
+    } else {
+      return 'active';
+    }
   }
 }
